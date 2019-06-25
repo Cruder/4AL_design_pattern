6d122127897242d284034d8c8d597975e36a49ee
Florian RICHARD <florianrichard96@gmail.com>
2019-06-25 15:12:11 +0200

    Units

---
+++ Source/Units/Wizard.cs
@@ -0,0 +1,25 @@
+namespace Source{
+    public class Wizard : IUnit
+    {
+        private int health;
+        private const int atkDamages = 10;
+        public Wizard(){
+            this.health = 5;
+        }
+
+        public void ReceiveDamages(int damages){
+            if(this.health > damages)
+                this.health -= damages;
+            else
+                this.health = 0;
+        }
+
+        public int Damages(){
+            return atkDamages;
+        }
+
+        public bool Alive(){
+            return(this.health > 0);
+        }
+    }
+}
